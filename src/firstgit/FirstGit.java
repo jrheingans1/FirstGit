6213e2b6204346438322a8bea97b93e22ff9eb61
jrheingans1 <jrheingans1@L11713038597.wctc.msft>
2013-01-24 16:54:56 -0600

    Changed my mind

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,13 +1,19 @@
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package firstgit;
 
-
+/**
+ *
+ * @author jrheingans1
+ */
 public class FirstGit {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // TODO code application logic here
     }
 }
