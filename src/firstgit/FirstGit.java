9b713f43acb60c16c285eb0a4ab020989b3e3c61
jrheingans1@my.wctc.edu <>
2013-01-24 17:01:13 -0600

    Added a Test Class and added a print statement

--- src/firstgit/FirstGit.java
+++ src/firstgit/FirstGit.java
@@ -1,6 +1,4 @@
-
 package firstgit;
-
 
 public class FirstGit {
 
@@ -9,5 +7,6 @@ public class FirstGit {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Good bye");
     }
 }
